40d8b3cd39d4971a142b6009b9e6849b5da20c0b
Vinicius <vinicius.paiva2@outlook.com>
2025-06-02 11:19:25 -0300

    Adicionado contador

--- script/script.js
+++ script/script.js
@@ -1,6 +1,10 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const gameOver = document.querySelector('.game-over')
+const elementoContador = document.getElementById("contador");
+
+let gameRunning = true;
+let contador = 0;
 
 const jump = () => {
  mario.classList.add('jump');
@@ -9,17 +13,26 @@ const jump = () => {
     }, 500);
 }
 
+async function contadorRodando() {
+    while (gameRunning) {
+        await new Promise(resolve => setTimeout(resolve, 1000)); // Espera 1 segundo
+        contador++;
+        elementoContador.textContent = contador * 100;
+    }
+}
+
+contadorRodando()
+
 const loop = setInterval(() => {
-    
     const pipePosition = pipe.offsetLeft;
     const marioPosition =+ window.getComputedStyle(mario).bottom.replace('px', '');
+
     if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
 
-       pipe.style.animation = 'none';
-       pipe.style.left = `${pipePosition}px`;
-
-       mario.style.animation = 'none';
-       mario.style.bottom = `${marioPosition}px`;
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
 
         mario.src = '/imagens/game-over.png';
         mario.style.width = '75px'
@@ -27,9 +40,9 @@ const loop = setInterval(() => {
 
         gameOver.style.display = ('flex')
 
+        gameRunning = false;
         clearInterval(loop);
     }
-
-}, 10);
+}, 100);
 
 document.addEventListener('keydown', jump);
